5f598424a4147cca1e5074ed8f15d1d0bf27b053
Lars Nordahl Bjørnbak <larsnb@getmail.no>
2024-01-11 15:14:52 +0100

    Update documentation and comments

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ The purpose of this repo is to test some basic concepts
 - Check function of lambda
 - Check FunctionalInterface
 - check customOperation
+- check effect of multiple uses of same interface
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,15 +9,31 @@ public class Main {
             int x, int y,
             BiFunction<Integer, Integer, Integer> operation,
             //Note to self. BiFunction takes to input values and one return value
+            // Also, "operation" is just a name for the function
             Function<Integer, String> converter) {
             // Note to self: Function only takes one
             // There are predefined Functions up to four inputs.
+            // Also the same for "converter"
         int result = operation.apply(x, y);
         return converter.apply(result);
     }
 
-    public static <T, U, V, W, X, R> R pentaFuncOperation(T t, U u, V v, W w, X x, PentaFunction<T, U, V, W, X, R> operation) {
+    // PentaFunction definition get from interface
+    public static <T, U, V, W, X, R> R usePentaFunction(
+            T t, U u, V v, W w, X x, PentaFunction<T, U, V, W, X, R> operation) {
         return operation.apply(t, u, v, w, x);
+    }
+
+    // A different implementation of PentaFunction
+    // Defines an inner static class named CustomPentaFunction.
+    static class CustomPentaFunction implements PentaFunction<Integer, Integer, Integer, Integer, Integer, Integer> {
+        // CustomPentaFunction implements the PentaFunction interface
+        // Must provide an implementation for the apply method declared in the interface.
+        // This method takes five Integer parameters (a, b, c, d, e) and returns an Integer.
+        @Override
+        public Integer apply(Integer a, Integer b, Integer c, Integer d, Integer e) {
+            return (a * b) + (c * d) - e;
+        }
     }
 
     public static void main(String[] args) {
@@ -40,11 +56,16 @@ public class Main {
         PentaFunction<Integer, Integer, Integer, Integer, Integer, Integer> customOperation =
                 (a, b, c, d, e) -> (a + b) * (c - d) / e;
 
-        String result = performOperation(num1, num2, multiply, intToString);
-        int result2 = pentaFuncOperation(num1, num2, num3, num4, num5, customOperation);
+        PentaFunction<Integer, Integer, Integer, Integer, Integer, Integer> customOperation2 =
+                new CustomPentaFunction();
 
-        System.out.println("Result1 as string: " + result);
-        System.out.println("Result2 of custom operation: " + result2);
+        String result = performOperation(num1, num2, multiply, intToString);
+        int result2 = usePentaFunction(num1, num2, num3, num4, num5, customOperation);
+        int result3 = usePentaFunction(10, 20, 3, 4, 5, customOperation2);
+
+        System.out.println("Result 1: as string: " + result);
+        System.out.println("Result 2: of custom operation: " + result2);
+        System.out.println("Result 3: " + result3);
 
     }
 }
